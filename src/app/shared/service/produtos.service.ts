fa001d2963dcb334886f59e4aca05aeeb04fac09
fernanda da lapa <fernandadalapa@hotmail.com>
2023-10-05 10:41:35 -0300

    Aula 7 - Ângular

--- src/app/shared/service/produtos.service.ts
+++ src/app/shared/service/produtos.service.ts
@@ -1,9 +1,33 @@
+import { Produto } from './produto';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Produto } from './model/produto';
+import { ProdutoSeletor } from '../model/seletor/produto.seletor';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProdutosService {
 
-  constructor() { }
+  private readonly API = 'http://localhost:8080/api/produtos';
+
+  constructor(private httpClient: HttpClient) { }
+
+listarTodos(): Observable <Array<Produto>> {
+  return this.httpClient.get<Array<Produto>>(this.API);
+}
+
+listarComSeletor(seletor: ProdutoSeletor) {
+  return this.httpClient.post<Array<Produto>>(this.API + "/filtro", seletor);
+}
+
+salvar(produto: Produto): Observable<Produto> {
+  return this.httpClient.post<Produto>(this.API, produto);
+}
+
+
+
+
 }

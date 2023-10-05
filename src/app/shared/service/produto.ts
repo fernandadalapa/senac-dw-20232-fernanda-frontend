1c8c6331531edeaabe82528a48fa2c279ba52553
fernanda da lapa <fernandadalapa@hotmail.com>
2023-10-05 10:43:06 -0300

    Aula 7 - Ângular

--- src/app/shared/service/produto.ts
+++ src/app/shared/service/produto.ts
@@ -1,10 +1,10 @@
 import { Fabricante } from './fabricante';
 
 export class Produto {
-    id: number;
+  id: number;
 	nome: string;
 	fabricanteDoProduto: Fabricante;
-    peso: number;
-    valor?: number; // O símbolo ? indica que o atributo é opcional
-    dataCadastro: Date;
+  peso: number;
+  valor?: number; // O símbolo ? indica que o atributo é opcional
+  dataCadastro: Date;
 }
